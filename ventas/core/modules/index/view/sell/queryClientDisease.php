7c58bb46f01ed1ada336347d3caa90d12ec4edef
djlaudio <luis.m24a@gmail.com>
2017-11-02 00:22:25 -0600

    Normal update

--- ventas/core/modules/index/view/sell/queryClientDisease.php
+++ ventas/core/modules/index/view/sell/queryClientDisease.php
@@ -11,20 +11,101 @@ $idClient= $_POST['idClient'];
 
 
 <div class="form-group">
-    <label for="facturas" class="col-lg-2 control-label">Facturas</label>
-    <div class="col-lg-10">
+    
+<table class="table table-bordered table-hover	">
+	<thead>
+		<th></th>
+		<th>Producto</th>
+		<th>Total</th>
+		<th>Fecha</th>
+		<th>Numero</th>
+		<th></th>
+	</thead>
 
-   
-    <select name="sell_id" class="form-control">
-    <option value="">-- NINGUNO --</option>
+
     <?php
 $find= mysqli_query($link,"SELECT * from sell WHERE person_id=$idClient and idDisease= $idDisease");
 while ($row = mysqli_fetch_array($find)) 
 	{?>
-      <option value="<?php echo $row['id'];?>"><?php echo $row['id2'];?></option>
+     <tr>
+		<td style="width:30px;">
+		<a href="index.php?view=onesell&id=<?php echo $sell->id; ?>" class="btn btn-xs btn-default"><i class="glyphicon glyphicon-eye-open"></i></a></td>
+
+		<td>
+
+<?php
+$operations = OperationData::getAllProductsBySellId($sell->id);
+echo count($operations);
+?>
+		<td>
+
+<?php
+$total= $sell->total-$sell->discount;
+	/*foreach($operations as $operation){
+		$product  = $operation->getProduct();
+		$total += $operation->q*$product->price_out;
+	}*/
+		echo "<b>‎₡ ".number_format($total)."</b>";
+
+?>			
+
+		</td>
+		<td><?php echo $sell->created_at; ?></td>
+
+		<td><?php echo $sell->id2; ?></td>
+
+
+<?php if($user->is_admin):?>
+   					
+    					
+		<td style="width:30px;"><a href="index.php?view=delsell&id=<?php echo $sell->id; ?>" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a></td>
+
+		
+		<?php endif; ?>
+
+		
+	</tr>
     <?php } ?>
       </select>
     </div>
   </div>
 
 
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<?php foreach($products as $sell):?>
+
+	
+
+<?php endforeach; ?>
