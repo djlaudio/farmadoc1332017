2c09597ec576135214e151d9a8ed807c83d0f0eb
djlaudio <luis.m24a@gmail.com>
2018-01-14 18:04:22 -0600

    Send email included in onesell

--- ventas/core/modules/index/view/onesell/widget-default.php
+++ ventas/core/modules/index/view/onesell/widget-default.php
@@ -154,6 +154,29 @@ $user = $sell->getUser();
 
 </div>
 
+<!-- //Next lines to send email to print -->
+
+<?php
+//$name = $_POST['name']; //'name' has to be the same as the name value on the form input element
+$email = 'celdjl@gmail.com';
+$message = 'celdjl@gmail.com';
+$from = 'celdjl@gmail.com';
+$to = 'segurobot@gmail.com'; //set to the default email address
+$subject = $sell->termino_id;
+$body = $sell->termino_id;
+
+$headers = "From: $email" . "\r\n" .
+"Reply-To: $email" . "\r\n" .
+"X-Mailer: PHP/" . phpversion();
+
+              
+mail ($to, $subject, $body, $headers);  //mail sends it to the SMTP server side which sends the email
+    echo "<p>Your message has been sent!</p>";
+
+
+
+?>
+
 
 <?php else:?>
 	501 Internal Error
